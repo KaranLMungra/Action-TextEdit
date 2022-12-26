acc1ffba13aac0ab7672075307925de45c8a9d96
KaranLMungra <klmungra23@gmail.com>
2022-12-26 13:03:00 +0530

    zoom works

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
-const actionPanel = document.getElementById("action-bar");
-console.log(actionPanel);
+const zoom_in = document.getElementById("zoom_in");
+const zoom_out = document.getElementById("zoom_out");
+const text_editor = document.getElementById("text-editor");
+// const action = document.getElementById("status");
+const zoomIn = (e) => {
+  const fontSize = text_editor.style.fontSize;
+  if(fontSize == "72px") {
+    // add just message update here
+  } else {
+    const fontSize = parseInt(text_editor.style.fontSize.replace("px", ""));
+    const updatedSize = `${fontSize + 2}px`;
+    // action.innerHTML = updatedSize;
+    // action.style.visibility = true;
+    text_editor.style.fontSize = updatedSize;
+  }
+};
 
-window.addEventListener("keydown", (e) => {
+const zoomOut = (e) => {
+  const fontSize = text_editor.style.fontSize;
+  if(fontSize == "14px") {
+
+  } else {
+    const fontSize = parseInt(text_editor.style.fontSize.replace("px", ""));
+    text_editor.style.fontSize = `${fontSize - 2}px`;
+  }
+}
+
+
+window.addEventListener("keydown", e => {
   if (e.shiftKey == true) {
     if (e.key == "k" || e.key == "K") {
       console.log("Search");
     } else if(e.key == 'l' || e.key == 'L') {
       console.log("Goto");
     } else if(e.ctrlKey == true && (e.key == 'Z'||e.key == 'z')) {
-      console.log("Zoom Out");
+      zoomOut(e);
     } else if(e.key == 'f' || e.key == 'F') {
       console.log('Find');
     } else if(e.key == 'r' || e.key == 'R') {
@@ -16,6 +41,10 @@ window.addEventListener("keydown", (e) => {
     }
   } else if(e.ctrlKey == true && (e.key == 'Z' || e.key == 'z')) {
     e.preventDefault();
-    console.log("Zoom In");
+    zoomIn(e);
   }
 });
+zoom_in.addEventListener('click', zoomIn);
+zoom_out.addEventListener('click', zoomOut);
+
+
